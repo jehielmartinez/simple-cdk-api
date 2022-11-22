b30738b0936c3a529e62432cd513e0bc1d6dd30e
jehielmartinez <jehiel.martinez@codexitos.com>
2022-11-21 22:31:07 -0600

    Initial commit

---
+++ test/unitec-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UnitecApi from '../lib/unitec-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/unitec-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UnitecApi.UnitecApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
